bd99f916dc2dd6411377747062ecc5d22f7b29f6
Rohit Singh <rohit99.ind@gmail.com>
2023-10-06 00:31:51 +0530

    feat: design

--- client/web/pages/_document.tsx
+++ client/web/pages/_document.tsx
@@ -1,13 +1,20 @@
-import * as React from 'react';
-import { Html, Head, Main, NextScript } from 'next/document';
+import * as React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
 // @ts-ignore
-import { AppRegistry } from 'react-native-web';
-import { flush } from '@gluestack-style/react';
+import { AppRegistry } from "react-native-web";
+import { flush } from "@gluestack-style/react";
 
 function Document() {
   return (
     <Html className="gs" lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Homemade+Apple&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -17,8 +24,8 @@ function Document() {
 }
 
 Document.getInitialProps = async ({ renderPage }: any) => {
-  AppRegistry.registerComponent('Main', () => Main);
-  const { getStyleElement } = AppRegistry.getApplication('Main');
+  AppRegistry.registerComponent("Main", () => Main);
+  const { getStyleElement } = AppRegistry.getApplication("Main");
   const page = await renderPage();
   const styles = [getStyleElement(), ...flush()];
   return { ...page, styles: React.Children.toArray(styles) };
--- client/web/pages/about.tsx
+++ client/web/pages/about.tsx
@@ -2,35 +2,127 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import { Box, Button, ButtonText, Input, InputField, Text } from "@gluestack-ui/themed";
-import ClientSDK from "@project/client-sdk";
+import {
+  AlertCircleIcon,
+  Box,
+  Button,
+  ButtonText,
+  FormControl,
+  FormControlError,
+  FormControlErrorIcon,
+  FormControlErrorText,
+  FormControlHelper,
+  FormControlHelperText,
+  FormControlLabel,
+  FormControlLabelText,
+  VStack,
+  Heading,
+  Input,
+  InputField,
+  Text,
+} from "@gluestack-ui/themed";
+// import ClientSDK from "@project/client-sdk";
 import { useEffect, useState } from "react";
 
-
 const About: NextPage = () => {
-  const [data, setData] = useState([])
-
-
+  const [data, setData] = useState([]);
 
   function test(data: any) {
-
     // const imageUrl = URL.createObjectURL(new Blob(binaryData, { type: "application/png" }))
-
     // const imageElement = document.createElement('img');
     // imageElement.src = imageUrl;
     // document.body.appendChild(imageElement);
   }
 
   return (
-    <div className={styles.parent}>
+    <Box overflow="scroll" flex={1}>
       <Box justifyContent="center" h="100vh" alignItems="center">
-        {data && data.map((value: any) => {
-          test(value)
-          return JSON.stringify(value)
-        })}
-        {/* {JSON.stringify(data)} */}
+        <VStack space="3xl">
+          <Box>
+            <Heading size="xl">Hi 👋,</Heading>
+            <Text>I'll need some details about you quickly.</Text>
+          </Box>
+          <FormControl isRequired size="md">
+            <FormControlLabel mb="$1">
+              <FormControlLabelText>Name</FormControlLabelText>
+            </FormControlLabel>
+            <Input variant="outline" size="md">
+              <InputField placeholder="Your Name, Please?" />
+            </Input>
+            {/* <FormControlHelper>
+            <FormControlHelperText>
+              Must be atleast 6 characters.
+            </FormControlHelperText>
+          </FormControlHelper> */}
+            <FormControlError>
+              <FormControlErrorIcon as={AlertCircleIcon} />
+              <FormControlErrorText>
+                This is a Required Field
+              </FormControlErrorText>
+            </FormControlError>
+          </FormControl>
+          <FormControl size="md">
+            <FormControlLabel mb="$1">
+              <FormControlLabelText>
+                Thoughts on React India 2023?
+              </FormControlLabelText>
+            </FormControlLabel>
+            <Input variant="outline" size="md">
+              <InputField placeholder="What did you like about the conference" />
+            </Input>
+            {/* <FormControlHelper>
+            <FormControlHelperText>
+              Must be atleast 6 characters.
+            </FormControlHelperText>
+          </FormControlHelper> */}
+            <FormControlError>
+              <FormControlErrorIcon as={AlertCircleIcon} />
+              <FormControlErrorText>
+                This is a Required Field
+              </FormControlErrorText>
+            </FormControlError>
+          </FormControl>
+          <FormControl isRequired size="md">
+            <FormControlLabel mb="$1">
+              <FormControlLabelText>Github</FormControlLabelText>
+            </FormControlLabel>
+            <Input variant="outline" size="md">
+              <InputField placeholder="Your Github ID" />
+            </Input>
+            {/* <FormControlHelper>
+            <FormControlHelperText>
+              Must be atleast 6 characters.
+            </FormControlHelperText>
+          </FormControlHelper> */}
+            <FormControlError>
+              <FormControlErrorIcon as={AlertCircleIcon} />
+              <FormControlErrorText>
+                This is a Required Field
+              </FormControlErrorText>
+            </FormControlError>
+          </FormControl>
+          <FormControl size="md">
+            <FormControlLabel mb="$1">
+              <FormControlLabelText>Email</FormControlLabelText>
+            </FormControlLabel>
+            <Input variant="outline" size="md">
+              <InputField placeholder="Your Email ID" />
+            </Input>
+            {/* <FormControlHelper>
+            <FormControlHelperText>
+              Must be atleast 6 characters.
+            </FormControlHelperText>
+          </FormControlHelper> */}
+            <FormControlError>
+              <FormControlErrorIcon as={AlertCircleIcon} />
+              <FormControlErrorText>
+                This is a Required Field
+              </FormControlErrorText>
+            </FormControlError>
+          </FormControl>
+        </VStack>
       </Box>
-    </div>
+    </Box>
   );
 };
 
